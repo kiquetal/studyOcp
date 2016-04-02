881e502d9c9096d96ef2b9b17f16cb4b9cb0ddf5
kiquetal <kiquetal@gmail.com>
2016-04-01 23:04:26 -0400

    Every start every pain

---
+++ NIO/ResolvingTest.java
@@ -0,0 +1,14 @@
+import java.nio.file.Paths
+public class ResolvingTest
+{
+ 
+
+  //Let's probe some variants about resolve and resolveSibling, 
+
+  public static void main(String args[])
+ {
+
+ }
+
+
+}
