012e6b541d1b28d31f57a891293892e3c1991b99
kiquetal <kiquetal@gmail.com>
2016-04-02 13:36:02 -0400

    iniciando mas operations

--- NIO/FilesOperations.java
+++ NIO/FilesOperations.java
@@ -1,12 +1,13 @@
 import java.nio.file.*;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 public class FilesOperations
 {
 //Create Directory  THROWS IOException, Root Folder must exists
-//Create Directories
-//Create File
-//Move File
-//Copy Files
+//Create Directories CREATE ALL EVEN IF THE ROOT FOLDER SPECIFIED IN THE ARGUMENT DOESN T EXISTS
+//Create File  MUST EXISTS CONTAINER FOLDER
+//Move File  
+//Copy Files 
 
 
  public static void main(String args[])
@@ -29,12 +30,25 @@ public class FilesOperations
    p1=Paths.get("test/direc");
    Files.createDirectory(p1);
  }
+ catch (FileAlreadyExistsException e)
+ {
+   System.out.println("la carpeta ya existe");
+ }
  catch(IOException ex) 
  {
+  
   assert false;
  }
-
-
+ try
+ {
+ //Doesn't check if root folder already exists!!!
+ p1=Paths.get("test3/bla2/bl4");
+ Files.createDirectories(p1);
+ }
+ catch(IOException e)
+ {
+  assert false:"No deberia llegar aqui por createDirectories";
+ }
 }
 
 
--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 #
   Esto será para probar que la existencia de muchas clases y muchas horas de prácticas, te pueden llegar al mismo lugar que iniciaste, o la nada.
 
+###
+  Es todo lo que quise que "persista" de lo que he leido
+  Mala Gupta (I love her)
+  K&B (I love Kathy)
+  Tutorial oficial (I hate the official documentation)
+  
 
 ## Tabla del dolor
 ===
-* NIO 
+* NIO
+  * Esto se reduce a Paths,Files, el truco esta en aprender, haciendo lanzar excepciones. 
